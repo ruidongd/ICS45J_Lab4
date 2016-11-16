9715e8ab66b138b4a497d837c3a296c957a20e4d
ruidongd <ruidongd@outlook.com>
2016-11-15 18:04:30 -0800

    setter/getter of PA

--- Lab4/ElevatorSimulation.java
+++ Lab4/ElevatorSimulation.java
@@ -1,8 +1,12 @@
 package Lab4;
+
+import java.util.Scanner;
+
 class ElevatorSimulation{
 	private int totalTime;
-	private int simUnitTime;
+	private int rateTime;
 	private BuildingManager bm;
+	
     public void start(){
 
     }
@@ -11,6 +15,10 @@ class ElevatorSimulation{
 
     }
     public void getConfig(){
+    	Scanner reader = new Scanner(System.in);
+    	totalTime = reader.nextInt();
+    	rateTime = reader.nextInt();
     	
+    	reader.close();
     }
 }
--- Lab4/PassengerArrival.java
+++ Lab4/PassengerArrival.java
@@ -1,1 +1,55 @@
 package Lab4;
+class PassengerArrival{
+	private int numPassengers;
+	private int destinationFloor;
+	private int timePeriod;
+	private int expectedTimeOfArrival;
+	public PassengerArrival()
+	{
+		numPassengers = 0;
+		destinationFloor = 0;
+		timePeriod = 0;
+		expectedTimeOfArrival = 0;
+	}
+
+	public int getNumPassengers()
+	{
+		return numPassengers;
+	}
+
+	public int getTimePeriod()
+	{
+		return timePeriod;
+	}
+
+	public int getDestinationFloor()
+	{
+		return destinationFloor;
+	}
+
+	public int getExpectedTimeOfArrival()
+	{
+		return expectedTimeOfArrival;
+	}
+
+	public void setNumPassengers(int num)
+	{
+		numPassengers = num;
+	}
+
+	public void setTimePeriod(int t)
+	{
+		timePeriod = t;
+	}
+
+	public void setDestinationFloor(int df)
+	{
+		destinationFloor = d;
+	}
+
+	public void setExpectedTimeOfArrival(int eta)
+	{
+		expectedTimeOfArrival = eta;
+	}
+
+}
